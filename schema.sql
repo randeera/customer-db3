799c5fb777a28ff92407dcbf7f10b99cb42ca14b
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-11-23 17:00:28 +0530

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS customer(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    contact VARCHAR(30) NOT NULL,
+    country VARCHAR(50) NOT NULL
+);
+
+INSERT INTO customer (first_name, last_name, contact, country) VALUES ('Kasun', 'Sampath', '077-1234567', 'Sri Lanka');
